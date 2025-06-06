12cea960d05e470d22916d6217561b0a69b67fe0
jituraj2 <jrajbher@gmail.com>
2025-06-06 13:23:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,3 +39,10 @@ def signin():
 def logout():
     session.pop('user', None)
     return redirect(url_for('signin'))
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
